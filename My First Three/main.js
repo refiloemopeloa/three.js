69e8030a3074d555b7920d506c801239c8a1e508
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-10-10 03:28:58 +0200

    seperated code into functions

--- My First Three/main.js
+++ My First Three/main.js
@@ -1,22 +1,34 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
+let scene, camera, renderer;
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let cube;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const init = () => {
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-        color: 0x00ff00
-    }
-);
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    scene = new THREE.Scene();
 
-camera.position.z = 5;
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    camera.position.z = 5;
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    addCube();
+    renderer.setAnimationLoop(animate);
+}
+
+const addCube = () => {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({
+            color: 0x777777
+        }
+    );
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+}
 
 function animate() {
     cube.rotation.x += 0.01;
@@ -24,4 +36,4 @@ function animate() {
     renderer.render(scene, camera);
 }
 
-renderer.setAnimationLoop(animate);
+init();
